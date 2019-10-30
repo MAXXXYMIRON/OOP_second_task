ba941de6815f79d8401d18add1e48915f63d9a5e
Maks <klavioturasucka2@gmail.com>
2019-10-30 21:30:35 +0900

    UpdateCode1.0

--- Class_matrix/Matrix.cpp
+++ Class_matrix/Matrix.cpp
@@ -1,12 +1,154 @@
 #include "Matrix.h"
 
-
-
+//Конструкторы класса
 Matrix::Matrix()
 {
 }
-
-
 Matrix::~Matrix()
 {
+}
+Matrix::Matrix(unsigned Row, unsigned Col)
+{
+
+}
+
+
+
+
+//Получить или установить эл. матрицы
+double Matrix::GetIndex(unsigned n, unsigned m)
+{
+
+}
+void Matrix::SetIndex(unsigned n, unsigned m, double Value)
+{
+
+}
+
+
+
+
+//Задание значений эл. матрицы
+//Все эл. одним значением
+void Matrix::FixValue(double Value)
+{
+
+}
+//Верхняя и нижняя грань рандомных значений для эл. матрицы
+//Все эл. разные
+void Matrix::RandValue(int FirstLim, int LastLim)
+{
+
+}
+
+
+
+
+//Присвоит полученое значение новой матрице
+Matrix Matrix::operator + (const Matrix& Matr2) const
+{
+
+}
+Matrix Matrix::operator - (const Matrix& Matr2) const
+{
+
+}
+Matrix Matrix::operator * (const Matrix& Matr2) const
+{
+
+}
+
+
+
+
+//Присвоит полученое значение новой матрице
+Matrix Matrix::Add(double Value)
+{
+
+}
+Matrix Matrix::Sub(double Value)
+{
+
+}
+Matrix Matrix::Mul(double Value)
+{
+
+}
+Matrix Matrix::Div(double Value)
+{
+
+}
+
+
+
+
+//Текущая матрица преобразуется в транспонированную, или диагональную, или обратную
+void Matrix::Transpose()
+{
+
+}
+void Matrix::Diagonal()
+{
+
+}
+void Matrix::Revers()
+{
+
+}
+
+//Вернет определитель матрицы, если та - квадратная
+double Matrix::Detr()
+{
+
+}
+
+
+
+
+//Получить или установить строку матрицы
+std::vector<double> Matrix::GetString(unsigned Row)
+{
+
+}
+void Matrix::SetString(unsigned Row, std::vector<double> Str)
+{
+
+}
+
+
+
+
+//Полученное значение запишет в новую матрицу
+void Matrix::operator += (const Matrix& Matr2)
+{
+
+}
+void Matrix::operator -= (const Matrix& Matr2)
+{
+
+}
+void Matrix::operator *= (const Matrix& Matr2)
+{
+
+}
+
+
+
+
+//Полученное значение запишет в новую матрицу
+void Matrix::operator += (double Value)
+{
+
+}
+void Matrix::operator -= (double Value)
+{
+
+}
+void Matrix::operator *= (double Value)
+{
+
+}
+void Matrix::operator /= (double Value)
+{
+
 }
--- Class_matrix/Matrix.h
+++ Class_matrix/Matrix.h
@@ -1,8 +1,61 @@
 #pragma once
+#include<iostream>
+#include <vector>
+
+typedef std::vector<std::vector<double>> matrix;
+
 class Matrix
 {
+private:
+	matrix Matr;
 public:
+	//Конструкторы класса
 	Matrix();
 	~Matrix();
+	Matrix(unsigned Row, unsigned Col);
+
+	//Получить или установить эл. матрицы
+	double GetIndex(unsigned n, unsigned m);
+	void SetIndex(unsigned n, unsigned m, double Value);
+
+	//Задание значений эл. матрицы
+	//Все эл. одним значением
+	void FixValue(double Value);
+	//Верхняя и нижняя грань рандомных значений для эл. матрицы
+	//Все эл. разные
+	void RandValue(int FirstLim, int LastLim);
+
+	//Присвоит полученое значение новой матрице
+	Matrix operator + (const Matrix& Matr2) const;
+	Matrix operator - (const Matrix& Matr2) const;
+	Matrix operator * (const Matrix& Matr2) const;
+
+	//Присвоит полученое значение новой матрице
+	Matrix Add(double Value);
+	Matrix Sub(double Value);
+	Matrix Mul(double Value);
+	Matrix Div(double Value);
+
+	//Текущая матрица преобразуется в транспонированную, или диагональную, или обратную
+	void Transpose();
+	void Diagonal();
+	void Revers();
+	//Вернет определитель матрицы, если та - квадратная
+	double Detr();
+
+	//Получить или установить строку матрицы
+	std::vector<double> GetString(unsigned Row);
+	void SetString(unsigned Row, std::vector<double> Str);
+
+	//Полученное значение запишет в новую матрицу
+	void operator += (const Matrix& Matr2);
+	void operator -= (const Matrix& Matr2);
+	void operator *= (const Matrix& Matr2);
+
+	//Полученное значение запишет в новую матрицу
+	void operator += (double Value);
+	void operator -= (double Value);
+	void operator *= (double Value);
+	void operator /= (double Value);
 };
 
